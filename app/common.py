26689109cc17be8ed9d63d3b1c2866fc42c141a2
Marina Flores Payan <71663779+marinafloresp@users.noreply.github.com>
2023-08-09 15:22:23 +0100

    Update common.py

--- app/common.py
+++ app/common.py
@@ -151,7 +151,7 @@ def alt_hor_barplot(df, x, y, num_cols, title_x, title_y, colors,main_title,save
 #Interactive vertical bar chart
 def alt_ver_barplot(df, x, y, num_cols, title_x, title_y, colors,main_title,save, tooltip):
     palette = colours(num_cols)
-    base = alt.Chart(df).mark_bar().encode(
+    base = alt.Chart(df).mark_bar(size=40).encode(
         alt.X(x +':N', title=title_x),
         alt.Y(y+':Q', title=title_y,axis=alt.Axis(grid=False)), alt.Color(colors+':N'), tooltip = tooltip
     ).properties(
@@ -167,14 +167,14 @@ def alt_scatterplot(df, x, y, title_x, title_y,main_title,save,tooltip ):
         x=alt.Y(x, title=title_x),
         y=alt.Y(y+':Q', title=title_y),
         tooltip=tooltip
-    ).interactive().properties(height=650, width = 600,title=main_title)
+    ).interactive().properties(height=400, width = 650,title=main_title)
     savePlot(base, save)
     st.altair_chart(base, theme="streamlit", use_container_width=True)
 
 #Interactive boxplot
 def alt_boxplot(df, x, y, num_cols, title_x, title_y, colors,main_title,save):
     palette = colours(num_cols)
-    base = alt.Chart(df, title="AAC response").mark_boxplot(extent='min-max', ticks=True, size=60).encode(
+    base = alt.Chart(df, title="AAC response").mark_boxplot(extent='min-max', ticks=True, size=50).encode(
         x=alt.X(x, title=title_x),
         y=alt.Y(y, title=title_y), color=alt.Color(colors + ':N')
     ).properties(height=650, width = 600, title = main_title).configure_range(category=alt.RangeScheme(palette))
@@ -200,7 +200,7 @@ def ecdf(data,intervals):
     return pct, pct_val
 
 #Applies ECDF for samples in a chosen basket-cluster interaction (Basket-Cluster interaction-Selected interaction page)
-def ecdf_interaction(basket,cluster,RawD, cred_inter):
+def ecdf_interaction(data,basket,cluster,RawD, cred_inter):
     intervals = np.array([100-cred_inter-5, 50, cred_inter+5])
     basket_index = data.baskets_names.index(basket)
     cluster_index = data.clusters_names.index(cluster)
